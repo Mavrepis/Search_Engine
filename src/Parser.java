8a57d8ac9c2c12ec9ce4b3ec9872f36c40158d21
Mavrepis <philipmavrepis@gmail.com>
2019-03-26 02:06:34 +0200

    Added infix to Postfix parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Parser {
 
@@ -29,6 +28,46 @@ class Parser {
             token_list.add(Token);
         }
         return token_list;
+    }
+
+    static String infix_to_Postfix(String infix){
+        HashMap<String,Integer> precedence = new HashMap<>();
+        precedence.put("~",3);
+        precedence.put("&",2);
+        precedence.put("|",1);
+        precedence.put("(",0);
+
+        Stack<String> opStack = new Stack<>();
+        List<String> postfixList = new ArrayList<>();
+        List<String> tokenList;
+        tokenList = tokenize(infix);
+        String top;
+
+        for (String token: tokenList){
+            if (Character.isLetterOrDigit(token.charAt(0))){
+                postfixList.add(token);
+            }
+            else if(token.equals("(")){
+                opStack.push(token);
+            }
+            else if(token.equals(")")){
+                top = opStack.pop();
+                while (top.equals("(")){
+                    postfixList.add(top);
+                    top = opStack.pop();
+                }
+            }
+            else{
+                while(!opStack.isEmpty() && (precedence.get(opStack.peek()) >= precedence.get(token) ) ) {
+                    postfixList.add(opStack.pop());
+                }
+                opStack.push(token);
+            }
+        }
+        while(!opStack.isEmpty()){
+            postfixList.add(opStack.pop());
+        }
+        return String.join(" ",postfixList);
     }
 
 
