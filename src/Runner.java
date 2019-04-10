3714f817348706198df393810e0aec2ee10661c6
Philip <philipmavrepis@>
2019-04-10 17:09:46 +0300

    Hashmap.ser->Dictionary.ser *Added* Calculate term frequency in each document Calculate document frequency (collection-wise) Create Inverted Index with tf-idf (tf * log(N/df)) scoring

--- src/Runner.java
+++ src/Runner.java
@@ -3,30 +3,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import static java.lang.Boolean.TRUE;
+
 
 public class Runner {
 
 
+    @SuppressWarnings("SuspiciousMethodCalls")
     public static void main(String[] args) {
         final long startTime = System.currentTimeMillis();
         Parser p = new Parser();
-
         Dictionary dict = new Dictionary();
         MapDocId2Files map = new MapDocId2Files(0);
 
-        File hash_map = new File("hashmap.ser");
+        File hash_map = new File("dictionary.ser");
         File map_docId = new File("map.ser");
 
         if (hash_map.exists() && map_docId.exists() ) {
             System.out.println("Loading dictionary!");
             try {
-                FileInputStream fis_hash = new FileInputStream("hashmap.ser");
+                FileInputStream fis_hash = new FileInputStream("dictionary.ser");
                 ObjectInputStream ois_hash = new ObjectInputStream(fis_hash);
                 dict = (Dictionary) ois_hash.readObject();
                 ois_hash.close();
@@ -48,17 +52,44 @@ public class Runner {
             }
 
         } else {
-            try (Stream<Path> walk = Files.walk(Paths.get("../data"))) {
+            try (Stream<Path> walk = Files.walk(Paths.get("C:\\Users\\Philip\\Documents\\IntelliJProjects\\Search_Engine\\data"))) {
 
                 List<String> result = walk.map(Path::toString)
                         .filter(f -> f.endsWith(".txt")).collect(Collectors.toList());
                 System.out.println("Creating dictionary!");
                 map = new MapDocId2Files(result.size());
-                for (String file : result) {
-                    ArrayList words = p.parseFile(file);
-                    map.add(result.indexOf(file),file);
+                HashMap<String,Float> df = new HashMap<>();
+
+                for (String path : result) {
+                    ArrayList words = p.parseFile(path);
+                    map.add(result.indexOf(path),path);
+                    HashMap<String,Integer> term_frequencies = new HashMap<>();
                     for (Object word : words) {
-                        dict.insert(word.toString(), result.indexOf(file));
+                        //noinspection SuspiciousMethodCalls
+                        if(!term_frequencies.containsKey(word)){
+                            term_frequencies.put(word.toString(),1);
+                            if(df.containsKey(word.toString())){
+                                df.put(word.toString(),df.get(word.toString())+1);
+                                continue;
+                            }
+                            df.put(word.toString(),1.0f);
+                        }
+                        else{
+                            //noinspection SuspiciousMethodCalls
+                            term_frequencies.put(word.toString(),term_frequencies.get(word)+1);
+                        }
+                    }
+                    for(Map.Entry<String, Integer> entry : term_frequencies.entrySet()) {
+                        String word = entry.getKey();
+                        float value = entry.getValue();
+                        dict.insert(word,result.indexOf(path),value);
+                    }
+                }
+                for(Map.Entry<String, Float> entry : df.entrySet()) {
+                    ArrayList<Posting> entry_posting = dict.get(entry.getKey());
+                    for(Posting posting :entry_posting){
+                        float current_score = posting.getScore();
+                        posting.setScore(current_score * (float)Math.log(8000/entry.getValue()));
                     }
                 }
 
@@ -68,7 +99,7 @@ public class Runner {
 
             try {
 
-                FileOutputStream fos = new FileOutputStream("hashmap.ser");
+                FileOutputStream fos = new FileOutputStream("dictionary.ser");
                 ObjectOutputStream oos = new ObjectOutputStream(fos);
                 oos.writeObject(dict);
                 oos.close();
@@ -79,14 +110,14 @@ public class Runner {
                 oos_map.writeObject(map);
                 oos_map.close();
                 fos_map.close();
-                System.out.println("Serialized HashMap and Map data is saved in hashmap.ser and map.ser");
+                System.out.println("Serialized HashMap and Map data is saved in dictionary.ser and map.ser");
 
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
         }
-        Queries q = new Queries(map);
-//        dict.print();
+//        Queries q = new Queries(map);
+        dict.print();
 //        //Get word's list of appearances.
 //        System.out.println(dict.get("response"));
 //
@@ -112,17 +143,17 @@ public class Runner {
 //                Queries.or(Queries.intersect(list_first_word,list_second_word),Queries.intersect(list_second_word,list_third_word))
 //        );
 //        System.out.println(q.n_word_and(list_first_word,list_second_word,list_third_word,list_fourth_word));
-        final long endTime = System.currentTimeMillis();
-        ExpressionParser et = new ExpressionParser();
-        System.out.println("Please enter your query like this: ( A & ~B ) | ( C & D )");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String query = br.readLine().toLowerCase();
-            map.print(et.evaluate_query(Parser.infix_to_Postfix(query),dict));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Total execution time: " + (endTime - startTime)*0.001 +" seconds");
+//        final long endTime = System.currentTimeMillis();
+//        ExpressionParser et = new ExpressionParser();
+//        System.out.println("Please enter your query like this: ( A & ~B ) | ( C & D )");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        try {
+//            String query = br.readLine().toLowerCase();
+//            map.print(et.evaluate_query(Parser.infix_to_Postfix(query),dict));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Total execution time: " + (endTime - startTime)*0.001 +" seconds");
     }
 
 }
