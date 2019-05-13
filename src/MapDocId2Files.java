83dd52cd1abed72ec3168df6c08dd05f57c003c1
Philip <philipmavrepis@>
2019-05-13 21:55:29 +0300

    Changed print method, now creates HTML file where query's results are stored.
    Opens directly to browser on runtime

--- src/MapDocId2Files.java
+++ src/MapDocId2Files.java
@@ -1,4 +1,7 @@
-import java.io.Serializable;
+import java.awt.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MapDocId2Files implements Serializable {
@@ -30,9 +33,34 @@ public class MapDocId2Files implements Serializable {
     }
     void print(int[] docs){
         if (docs!=null && docs.length!=0) {
-            for (int docId : docs) {
-                System.out.println(get(docId));
+            String file="output.html";
+            File htmlfile = new File(file);
+            StringBuilder sb = new StringBuilder();
+            try {
+                Files.deleteIfExists(Paths.get("../output.html"));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+                try {
+                    for (int docId : docs) {
+                        sb.append("<a href=\"");
+                        sb.append(get(docId).substring(57).replace("\\","/"));
+                        sb.append("\">");
+                        sb.append(get(docId));
+                        sb.append("</a>");
+                        sb.append(System.getProperty("line.separator"));
+                    }
+                    bw.write("<html><head><title>New Page</title></head><body>" +
+                            "<p>"+sb+"</p>" +
+                            "</body></html>");
+                    bw.close();
+                    Desktop.getDesktop().browse(htmlfile.toURI());
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
         }
         else System.out.println("No results");
     }
